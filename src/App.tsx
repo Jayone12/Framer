5b9b62f1c339e2a7d1aeb4497cb148c907619916
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-21 16:15:48 +0900

    feat: Drag 및 영역내 Drag 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -22,12 +33,27 @@ const Box = styled(motion.div)`
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgba(46, 204, 113)", transition: { duration: 1 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          // 드래그가 종료되면 시작지점으로 이동시킨다.
+          dragSnapToOrigin
+          // 드래그 되는 요소가 영역 밖으로 나갔을때의 당기는 힘 설정
+          dragElastic={0.5}
+          // Drag 영역을 지정할 수 있다.
+          // 부모 요소의 레퍼런스를 만들고 이걸로 영역을 지정한다.
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
